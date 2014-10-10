a9d81e96b386e8bf2b7234f395fdf6e1fd270658
Truong Nam Kien <kien@trinetcloud.com>
2014-10-10 10:32:34 +0700

    Update Search page

--- application/modules/search/views/search_list_view.php
+++ application/modules/search/views/search_list_view.php
@@ -1,32 +1,27 @@
-<?php echo asset_link_tag('css/category.css', 'stylesheet', 'text/css'); ?>
+<?php echo asset_link_tag('css/categories.css', 'stylesheet', 'text/css'); ?>
 
-<?php echo Modules::run('category/_pagelet_category_list'); ?>
+<div id="catgoryHeaderContainer" class="categoryTemplate">
+	<div id="categoryHeader">
+		<h1 class="search-large-text"><?php echo lang('search_result_desc', '', $query); ?></h1>
+	</div>
+	<div class="clear"></div>
 
-<div class="product_category">
-    <?php echo Modules::run('product/_pagelet_hot_products'); ?>
-
-    <div class="w_category_products">
-        <div class="hotoffers">
-            <h3>
-                <?php echo lang('search_result_desc', '', $query); ?>
-            </h3>
-        </div>
-        <div class="clear"></div>
-
-        <div class="listproduct">
-            <div class="product" id="product_list">
-                <?php if (empty($products)): ?>
-                    <div class="tac fs20 ma15"><?php echo lang('search_result_empty'); ?></div>
-                <?php else: ?>
-                    <?php $index = 0; ?>
-                    <?php foreach ($products as $product): ?>
-                        <div class="item <?php echo (($index + 1) % 4 == 0 ? 'last' : ''); ?>">
-                            <?php echo Modules::run('product/_pagelet_product_item', $product); ?>
-                        </div>
-                        <?php $index++; ?>
-                    <?php endforeach; ?>                
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
+	<div id="Main" class="mainSize2 L1L2-template templateRender">
+		<div id="offers">
+			<div id="product_list">
+				<?php if (empty($products)): ?>
+					<div class="tac fs20 ma15"><?php echo lang('search_result_empty'); ?></div>
+				<?php else: ?>
+					<?php $index = 0; ?>
+					<?php foreach ($products as $product): ?>
+						<div class="coupon <?php echo (($index + 1) % 4 == 0 ? '' : 'tileborderright'); ?>">
+							<?php echo Modules::run('product/_pagelet_product_item', $product); ?>
+						</div>
+						<?php $index++; ?>
+					<?php endforeach; ?>                
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
 </div>
+
