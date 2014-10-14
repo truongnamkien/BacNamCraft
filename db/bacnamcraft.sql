93eb1f2f8d59abc7762f84dee650280274349bbb
Truong Nam Kien <kien@trinetcloud.com>
2014-10-14 09:52:39 +0700

    Update db structure

--- db/bacnamcraft.sql
+++ db/bacnamcraft.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 3.5.8.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 08, 2014 at 12:21 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Host: localhost
+-- Generation Time: Oct 13, 2014 at 10:27 PM
+-- Server version: 5.0.96
+-- PHP Version: 5.2.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `bncraft`
+-- Database: `db_bncraft`
 --
 
 -- --------------------------------------------------------
@@ -27,12 +27,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
-  `session_id` varchar(40) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
-  `ip_address` varchar(16) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
-  `user_agent` varchar(120) COLLATE utf8_unicode_ci NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text COLLATE utf8_unicode_ci,
-  PRIMARY KEY (`session_id`)
+  `session_id` varchar(40) collate utf8_unicode_ci NOT NULL default '0',
+  `ip_address` varchar(16) collate utf8_unicode_ci NOT NULL default '0',
+  `user_agent` varchar(120) collate utf8_unicode_ci NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL default '0',
+  `user_data` text collate utf8_unicode_ci,
+  PRIMARY KEY  (`session_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -42,12 +42,12 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 --
 
 CREATE TABLE IF NOT EXISTS `contact` (
-  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_id` int(11) NOT NULL auto_increment,
   `email` varchar(100) NOT NULL,
   `content` text NOT NULL,
   `ip_address` varchar(20) NOT NULL,
   `created_at` int(11) NOT NULL,
-  PRIMARY KEY (`contact_id`),
+  PRIMARY KEY  (`contact_id`),
   KEY `email` (`email`,`ip_address`,`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -58,13 +58,14 @@ CREATE TABLE IF NOT EXISTS `contact` (
 --
 
 CREATE TABLE IF NOT EXISTS `content` (
-  `content_id` int(11) NOT NULL AUTO_INCREMENT,
+  `content_id` int(11) NOT NULL auto_increment,
   `url` varchar(255) NOT NULL,
   `content_vietnamese` text,
+  `content_english` text NOT NULL,
   `page_id` int(11) NOT NULL,
   `layout` int(11) NOT NULL,
   `display_order` int(11) NOT NULL,
-  PRIMARY KEY (`content_id`),
+  PRIMARY KEY  (`content_id`),
   UNIQUE KEY `display_order` (`display_order`),
   KEY `content_category_id` (`page_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -76,10 +77,10 @@ CREATE TABLE IF NOT EXISTS `content` (
 --
 
 CREATE TABLE IF NOT EXISTS `customer_email` (
-  `customer_email_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_email_id` int(11) NOT NULL auto_increment,
   `email` varchar(255) NOT NULL,
-  `unsubscribed` enum('Active','Inactive') NOT NULL DEFAULT 'Inactive',
-  PRIMARY KEY (`customer_email_id`),
+  `unsubscribed` enum('Active','Inactive') NOT NULL default 'Inactive',
+  PRIMARY KEY  (`customer_email_id`),
   UNIQUE KEY `email` (`email`),
   KEY `unsubscribed` (`unsubscribed`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -91,18 +92,17 @@ CREATE TABLE IF NOT EXISTS `customer_email` (
 --
 
 CREATE TABLE IF NOT EXISTS `order` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `fullname` varchar(50) DEFAULT NULL,
+  `order_id` int(11) NOT NULL auto_increment,
+  `email` varchar(255) default NULL,
+  `fullname` varchar(50) default NULL,
   `mobile` varchar(15) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `delivery_time` varchar(20) DEFAULT NULL,
   `note` text,
-  `discount` int(11) DEFAULT NULL,
+  `discount` int(11) default NULL,
   `discount_note` text,
   `created_at` int(11) NOT NULL,
-  `status` enum('pending','progressing','finished','cancelled') NOT NULL DEFAULT 'pending',
-  PRIMARY KEY (`order_id`),
+  `status` enum('pending','progressing','finished','cancelled') NOT NULL default 'pending',
+  PRIMARY KEY  (`order_id`),
   KEY `email` (`email`,`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -113,12 +113,12 @@ CREATE TABLE IF NOT EXISTS `order` (
 --
 
 CREATE TABLE IF NOT EXISTS `order_detail` (
-  `order_detail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_detail_id` int(11) NOT NULL auto_increment,
   `order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `price` int(11) NOT NULL,
-  PRIMARY KEY (`order_detail_id`),
+  PRIMARY KEY  (`order_detail_id`),
   KEY `order_id` (`order_id`,`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -129,12 +129,13 @@ CREATE TABLE IF NOT EXISTS `order_detail` (
 --
 
 CREATE TABLE IF NOT EXISTS `page` (
-  `page_id` int(11) NOT NULL AUTO_INCREMENT,
+  `page_id` int(11) NOT NULL auto_increment,
   `name_vietnamese` varchar(100) NOT NULL,
+  `name_english` varchar(100) NOT NULL,
   `display_order` int(11) NOT NULL,
-  `status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
-  `parent_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`page_id`),
+  `status` enum('Active','Inactive') NOT NULL default 'Active',
+  `parent_id` int(11) default NULL,
+  PRIMARY KEY  (`page_id`),
   UNIQUE KEY `display_order` (`display_order`),
   KEY `status` (`status`),
   KEY `parent_id` (`parent_id`),
@@ -148,18 +149,20 @@ CREATE TABLE IF NOT EXISTS `page` (
 --
 
 CREATE TABLE IF NOT EXISTS `product` (
-  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL auto_increment,
   `name_vietnamese` varchar(100) NOT NULL,
+  `name_english` varchar(100) NOT NULL,
   `description_vietnamese` text NOT NULL,
+  `description_english` text NOT NULL,
   `price` int(11) NOT NULL,
-  `price_off` int(11) DEFAULT NULL,
+  `price_off` int(11) default NULL,
   `product_category_id` int(11) NOT NULL,
   `display_order` int(11) NOT NULL,
-  `status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
+  `status` enum('Active','Inactive') NOT NULL default 'Active',
   `created_at` int(11) NOT NULL,
-  `hot` enum('Active','Inactive') NOT NULL DEFAULT 'Inactive',
-  `sold_out` enum('Active','Inactive') NOT NULL DEFAULT 'Inactive',
-  PRIMARY KEY (`product_id`),
+  `hot` enum('Active','Inactive') NOT NULL default 'Inactive',
+  `sold_out` enum('Active','Inactive') NOT NULL default 'Inactive',
+  PRIMARY KEY  (`product_id`),
   UNIQUE KEY `display_order` (`display_order`),
   KEY `product_category_id` (`product_category_id`),
   KEY `status` (`status`),
@@ -175,12 +178,13 @@ CREATE TABLE IF NOT EXISTS `product` (
 --
 
 CREATE TABLE IF NOT EXISTS `product_category` (
-  `product_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_category_id` int(11) NOT NULL auto_increment,
   `category_name_vietnamese` varchar(100) NOT NULL,
+  `category_name_english` varchar(100) NOT NULL,
   `display_order` int(11) NOT NULL,
-  `parent_id` int(11) DEFAULT NULL,
-  `status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
-  PRIMARY KEY (`product_category_id`),
+  `parent_id` int(11) default NULL,
+  `status` enum('Active','Inactive') NOT NULL default 'Active',
+  PRIMARY KEY  (`product_category_id`),
   UNIQUE KEY `display_order` (`display_order`),
   KEY `parent_id` (`parent_id`),
   KEY `status` (`status`)
@@ -193,10 +197,10 @@ CREATE TABLE IF NOT EXISTS `product_category` (
 --
 
 CREATE TABLE IF NOT EXISTS `product_photo` (
-  `product_photo_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_photo_id` int(11) NOT NULL auto_increment,
   `url` varchar(255) NOT NULL,
   `product_id` int(11) NOT NULL,
-  PRIMARY KEY (`product_photo_id`),
+  PRIMARY KEY  (`product_photo_id`),
   KEY `product_id` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -207,16 +211,12 @@ CREATE TABLE IF NOT EXISTS `product_photo` (
 --
 
 CREATE TABLE IF NOT EXISTS `static_content` (
-  `static_content_id` int(11) NOT NULL AUTO_INCREMENT,
+  `static_content_id` int(11) NOT NULL auto_increment,
   `page` varchar(40) NOT NULL,
   `content_name` varchar(50) NOT NULL,
   `content` text NOT NULL,
   `type` varchar(40) NOT NULL,
-  PRIMARY KEY (`static_content_id`),
+  PRIMARY KEY  (`static_content_id`),
   KEY `page` (`page`,`content_name`),
   KEY `type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
